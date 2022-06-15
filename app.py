7d1338846ea54927c4488aeb6bac2b9b2bd7fbda
Semo <tpah20@gmail.com>
2022-06-15 15:55:46 +0900

    global client_num, status 위치 변경

--- app.py
+++ app.py
@@ -137,12 +137,13 @@ def get_info():
 
 @app.get("/start/{Server_IP}")
 async def main(Server_IP : str) -> None:
+    global client_num, status
 
     print('FL server start')
     status.FL_client_start = True
 
     try:
-        global client_num, status
+        
         
         status.FL_server_IP = Server_IP
         print("server_ip: ", status.FL_server_IP)
