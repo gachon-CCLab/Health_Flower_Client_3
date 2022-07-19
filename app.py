b91f4c9dbcc516ef4bb500b564e932f6b60e7b37
Semo <tpah20@gmail.com>
2022-07-19 15:59:28 +0900

    2

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ config.verify_ssl=True
 api_client = client.CoreV1Api(client.ApiClient(config))
 
 # 첫 번째 argument에 당신이 사용하는 namespace를 입력한다
-ret = api_client.list_namespaced_pod("namespace 입력", watch=False)
+ret = api_client.list_namespaced_pod("fed-repl-mjh", watch=False)
 
 print("Listing pods with their IPs:")
 
